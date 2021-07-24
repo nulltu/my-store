3709af8dc573f6fd7fa2a99215df5cfa4e9795cb
nulltu <rusbent.matta@gmail.com>
2021-07-24 18:21:44 -0300

    itemCount

---
+++ src/components/itemCount/ItemCount.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ItemCount() {
+    return (
+        <div>
+            vvi
+        </div>
+    )
+}
+
+export default ItemCount
--- src/pages/Products/Products.jsx
+++ src/pages/Products/Products.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import ItemCount from '../../components/itemCount/ItemCount'
 import ItemListContainer from '../../components/itemListContainer'
 
 function Products() {
     return (
         <div>
             <ItemListContainer />
+            <ItemCount />
         </div>
     )
 }
